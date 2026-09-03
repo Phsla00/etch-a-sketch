99cb28a3e05ae3082cd689118402e7a5c14344d3
Paulo Almeida <145488386+Phsla00@users.noreply.github.com>
2026-09-03 10:45:30 -0300

    Add a function to paint the canvas

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const DEFAULT_SIZE = 16;
-const DEFAULT_COLOR = "black";
+const DEFAULT_COLOR = "#000";
+const DEFAULT_MODE = "color";
 
 let currentSize = DEFAULT_SIZE;
 let currentColor = DEFAULT_COLOR;
+let currentMode = "color"
 
 const htmlCanvas = document.querySelector("#canvas");
 
@@ -17,8 +19,22 @@ function createCanvas(size) {
     }
     
     canvas.map((canva) => {
+        canva.addEventListener("mouseover", paintCanvas);
+        canva.addEventListener("mousedown", paintCanvas);
         htmlCanvas.appendChild(canva);
-    })
+    });
+}
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function paintCanvas(e) {
+    if (e.type === "mouseover" && !mouseDown) return;
+    if (currentMode === "color") {
+        e.target.style.backgroundColor = currentColor;
+    }
+    
 }
 
 createCanvas(currentSize);
